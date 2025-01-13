02b09c6e9e477453037728889bf9d2e0900b1e36
simondeient <95246102+simondeient@users.noreply.github.com>
2025-01-13 23:46:53 +0100

    Delete HomePage.js

--- HomePage.js
+++
@@ -1,9 +0,0 @@
-/* jshint esversion: 6, browser: true */
-
-(function() {
-    'use strict';
-    
-    window.addEventListener('DOMContentLoaded', () => {
-        alert('Welcome to Film Tourism! Start your journey into the world of cinema.');
-    });
-})();
